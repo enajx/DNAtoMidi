84e2fc0b2fbcf929dac4b3fbd834cb4a0c0a3a8a
Elias <30030348+AjarAilerons@users.noreply.github.com>
2019-03-16 23:29:50 +0100

    bug fix

--- DNAtomidi.py
+++ DNAtomidi.py
@@ -18,7 +18,7 @@ def formatDNAFile(rawFile):
 
 def textToPitches(dnaText, startingPitch):
     degrees = []
-    speeds = []p
+    speeds = []
     notesInKey = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
 
